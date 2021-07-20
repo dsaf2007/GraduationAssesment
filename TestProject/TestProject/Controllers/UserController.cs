1ebb8b425fe5488a0814a2a80be2ca31b7bb9d7c
unknown <dsaf2007@dongguk.edu>
2021-07-20 15:55:30 +0900

    including excel reader

--- TestProject/TestProject/Controllers/UserController.cs
+++ TestProject/TestProject/Controllers/UserController.cs
@@ -14,7 +14,8 @@ namespace ReadExcel.Controllers
         public IActionResult Index()
         {
             List<UserModel> users = new List<UserModel>();
-            var filename = "./wwwroot/upload/Users.xlsx";
+            List<classnum> classList = new List<classnum>();
+            var filename = "./wwwroot/upload/test2.xlsx";
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
             using (var stream = System.IO.File.Open(filename, System.IO.FileMode.Open, System.IO.FileAccess.Read))
@@ -23,12 +24,57 @@ namespace ReadExcel.Controllers
                 {
                     while(reader.Read())
                     {
+                        string[] value_arr=new string[5];
+                        for(int i = 0; i< 5;i++)
+                        {
+                            if (reader.GetValue(i) == null)
+                                value_arr[i] = "";
+                            else
+                                value_arr[i] = reader.GetValue(i).ToString();
+                        }
                         users.Add(new UserModel
                         {
-                            Name = reader.GetValue(0).ToString(),
-                            Email = reader.GetValue(1).ToString(),
-                            Phone = reader.GetValue(2).ToString()
+                            A = value_arr[0],
+                            B = value_arr[1],
+                            C = value_arr[2],
+                            D = value_arr[3],
+                            E = value_arr[4]
                         });
+                    }
+                    reader.NextResult();
+                    while (reader.Read())
+                    {
+                        string[] value_arr = new string[5];
+                        for (int i = 0; i < 5; i++)
+                        {
+                            if (reader.GetValue(i) == null)
+                                value_arr[i] = "";
+                            else
+                                value_arr[i] = reader.GetValue(i).ToString();
+                        }
+                        users.Add(new UserModel
+                        {
+                            A = value_arr[0],
+                            B = value_arr[1],
+                            C = value_arr[2],
+                            D = value_arr[3],
+                            E = value_arr[4]
+                        });
+                        //string[] value_arr = new string[4];
+                        //for (int i = 0; i < 4; i++)
+                        //{
+                        //    if (reader.GetValue(i) == null)
+                        //        value_arr[i] = "";
+                        //    else
+                        //        value_arr[i] = reader.GetValue(i).ToString();
+                        //}
+                        //classList.Add(new classnum
+                        //{
+                        //    num = value_arr[0],
+                        //    name = value_arr[1],
+                        //    grade = value_arr[2],
+                        //    year = value_arr[3]
+                        //});
                     }
                 }
             }
--- TestProject/TestProject/Models/UserModel.cs
+++ TestProject/TestProject/Models/UserModel.cs
@@ -7,8 +7,18 @@ namespace ReadExcel.Models
 {
     public class UserModel
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+        public string E { get; set; }
+    }
+
+    public class classnum
+    {
+        public string num { get; set; }
+        public string name { get; set; }
+        public string grade { get; set; }
+        public string year { get; set; }
     }
 }
