34b0a9e9e4182cdd3408ff0eab34f47c5de15b77
unknown <dsaf2007@dongguk.edu>
2021-07-21 16:17:25 +0900

    reading multiple sheets

--- TestProject/TestProject/Controllers/UserController.cs
+++ TestProject/TestProject/Controllers/UserController.cs
@@ -15,7 +15,7 @@ namespace ReadExcel.Controllers
         {
             List<UserModel> users = new List<UserModel>();
             List<classnum> classList = new List<classnum>();
-            var filename = "./wwwroot/upload/test2.xlsx";
+            var filename = "./wwwroot/upload/testtest.xlsx";
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
             using (var stream = System.IO.File.Open(filename, System.IO.FileMode.Open, System.IO.FileAccess.Read))
@@ -32,53 +32,60 @@ namespace ReadExcel.Controllers
                             else
                                 value_arr[i] = reader.GetValue(i).ToString();
                         }
-                        users.Add(new UserModel
+                        if (value_arr[4] == "단수")
                         {
-                            A = value_arr[0],
-                            B = value_arr[1],
-                            C = value_arr[2],
-                            D = value_arr[3],
-                            E = value_arr[4]
-                        });
+                            users.Add(new UserModel
+                            {
+                                A = value_arr[0],
+                                B = value_arr[1],
+                                C = value_arr[2],
+                                D = value_arr[3],
+                                E = value_arr[4]
+                            });
+                        }
                     }
                     reader.NextResult();
                     while (reader.Read())
                     {
-                        string[] value_arr = new string[5];
-                        for (int i = 0; i < 5; i++)
-                        {
-                            if (reader.GetValue(i) == null)
-                                value_arr[i] = "";
-                            else
-                                value_arr[i] = reader.GetValue(i).ToString();
-                        }
-                        users.Add(new UserModel
-                        {
-                            A = value_arr[0],
-                            B = value_arr[1],
-                            C = value_arr[2],
-                            D = value_arr[3],
-                            E = value_arr[4]
-                        });
-                        //string[] value_arr = new string[4];
-                        //for (int i = 0; i < 4; i++)
+                        //string[] value_arr = new string[5];
+                        //for (int i = 0; i < 5; i++)
                         //{
                         //    if (reader.GetValue(i) == null)
                         //        value_arr[i] = "";
                         //    else
                         //        value_arr[i] = reader.GetValue(i).ToString();
                         //}
-                        //classList.Add(new classnum
+                        //if (value_arr[4] == "목록")
                         //{
-                        //    num = value_arr[0],
-                        //    name = value_arr[1],
-                        //    grade = value_arr[2],
-                        //    year = value_arr[3]
-                        //});
+                        //    users.Add(new UserModel
+                        //    {
+                        //        A = value_arr[0],
+                        //        B = value_arr[1],
+                        //        C = value_arr[2],
+                        //        D = value_arr[3],
+                        //        E = value_arr[4]
+                        //    });
+                        //}
+                        string[] value_arr = new string[4];
+                        for (int i = 0; i < 4; i++)
+                        {
+                            if (reader.GetValue(i) == null)
+                                value_arr[i] = "";
+                            else
+                                value_arr[i] = reader.GetValue(i).ToString();
+                        }
+                        classList.Add(new classnum
+                        {
+                            num = value_arr[0],
+                            name = value_arr[1],
+                            grade = value_arr[2],
+                            year = value_arr[3]
+                        });
                     }
                 }
             }
-            return View(users);
+            var t = new Tuple<IEnumerable<UserModel>, IEnumerable<classnum>>(users, classList) { };
+            return View(t);
         }
     }
 }
