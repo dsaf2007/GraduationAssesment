2b9078b5384b4a3d39c6ff74e845a1f59d94cc51
unknown <dsaf2007@dongguk.edu>
2021-08-30 19:43:46 +0900

    template upload

---
+++ TestProject/TestProject/Controllers/AdminController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReadExcel.Models;
+using ExcelDataReader;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+
+namespace TestProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IWebHostEnvironment environment;
+        public AdminController(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Default GET method
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload(ICollection<IFormFile> fileCollection)//파일 업로드
+        {
+            var uploadDirectoryPath = Path.Combine(this.environment.WebRootPath, "upload" + Path.DirectorySeparatorChar);
+
+            foreach (IFormFile formFile in fileCollection)
+            {
+                if (formFile.Length > 0)
+                {
+                    string fileName = Path.GetFileName
+                    (
+                        ContentDispositionHeaderValue.Parse(formFile.ContentDisposition).FileName.Value
+                    );
+                    using (FileStream stream = new FileStream(Path.Combine(uploadDirectoryPath, fileName), FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            // return View();
+            return RedirectToAction("Index", "Admin");
+        }
+    }
+}
